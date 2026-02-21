1a2e909fb899b264db326856a5363be66c9256d1
Zrp <zrp0x0@gmail.com>
2026-02-21 21:48:14 +0900

    개발 환경 구축 및 기본적인 구조 설정 완료

--- src/main/java/com/club/manager/member/application/MemberService.java
+++ src/main/java/com/club/manager/member/application/MemberService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.club.manager.common.exception.BusinessException;
+import com.club.manager.common.exception.ErrorCode;
 import com.club.manager.member.domain.Member;
 import com.club.manager.member.domain.MemberRepository;
 import com.club.manager.member.domain.dto.MemberCreateRequest;
@@ -21,16 +23,24 @@ public class MemberService {
 
     @Transactional
     public MemberResponse registerMember(MemberCreateRequest request) {
+        // 중복 이메일 가입 방지 (DB에 이미 unique가 걸려있지만, Early Return)
+        if (memberRepository.existsByEmail(request.email())) {
+            throw new BusinessException(ErrorCode.EMAIL_ALREADY_EXISTS);
+        }
+
         Member member = request.toEntity();
         Member savedMember = memberRepository.save(member);
+
         return MemberResponse.from(savedMember);
     }
 
     @Transactional
     public List<MemberResponse> getAllMembers() {
         return memberRepository.findAll().stream()  
-            .map(MemberResponse::from)
-            .toList();
+                .map(MemberResponse::from)
+                .toList();
     }
+
+
 
 }
--- src/main/java/com/club/manager/member/domain/MemberRepository.java
+++ src/main/java/com/club/manager/member/domain/MemberRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     
+    // email 필드값이 파라미터와 똑같은 데이터가 존재하는지 확인
+    boolean existsByEmail(String email);
 }
